11c400ed7c2a9f04d41845b3fd122dc8c68207dc
VerboseAsync <71553967+VikashhAdaikalavan@users.noreply.github.com>
2026-01-27 22:31:21 +0530

    ...

--- Week 3/prog3.c
+++ Week 3/prog3.c
@@ -1,31 +1,56 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <math.h>
 
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
 
-int maxi(int a, int b) {
-    return (a > b) ? a : b;
+int caneat(int x, int n, int arr[n], int h)
+{
+    int tothrs = 0;
+    for (int i = 0; i < n; i++)
+    {
+        tothrs += ceil((double)(arr[i]) / x);
+    }
+    return tothrs <= h;
 }
 
 int main()
 {
     int n;
-    scanf("%d",&n);
-    
-    int max = -10000;
+    int h;
+    scanf("%d", &n);
+    scanf("%d", &h);
     int arr[n];
-    for(int i = 0; i< n ; i++) 
+    int r = INT_MIN;
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d",&arr[i]);
-        max = maxi(max,arr[i]);
+        scanf("%d", &arr[i]);
+        r = max(r, arr[i]);
     }
-    int r = max;
-    int l = 0;
-
-    while(l<=r)
+    int l = 1;
+    if (r == 0)
     {
-        int mid = (l+r)/2;
-        
+        printf("0");
+        return 0;
     }
 
-    
+    while (l <= r)
+    {
+        int mid = (l + r) / 2;
+        if (caneat(mid, n, arr, h))
+        {
+            r = mid - 1;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+
+    printf("%d\n", l);
+    return 0;
 }
