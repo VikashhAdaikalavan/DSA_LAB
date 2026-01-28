f053264e23038af02d24e9194130e72760bc419a
VikashhAdaikalavan <vikashhadaikalavan@gmail.com>
2026-01-28 22:00:55 +0530

    ...

--- Week 3/prog2.c
+++ Week 3/prog2.c
@@ -1,31 +1,32 @@
-#include<stdio.h>
-#include <stdlib.h>
+#include <stdio.h>
 #include <limits.h>
 
 int main()
 {
     int n;
-    scanf("%d",&n);
-    if(n == 0) return 0;
-    int l = 1;
-    int r = n;
-    int arr[n+2];
-    arr[0] = INT_MIN;
-    arr[n+1] = INT_MIN;
-    for(int i = 1; i<= n ; i++) scanf("%d",&arr[i]);
+    scanf("%d", &n);
+    if (n == 0) return 0;
 
-    while(l<=r)
+    int arr[n + 2];
+    arr[0] = INT_MIN;
+    arr[n + 1] = INT_MIN;
+
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &arr[i]);
+
+    int l = 1, r = n;
+
+    while (l < r)
     {
-        int mid = (l+r)/2;
-        if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1])
-        {
-            printf("%d",mid-1);
-            return 0;
-        }
-        else if(arr[mid]>=arr[mid-1] && arr[mid]<=arr[mid+1]) l = mid+1;
-        else if(arr[mid]<=arr[mid-1] && arr[mid]>=arr[mid+1]) r= mid-1;
+        int mid = (l + r) / 2;
+
+        if (arr[mid] > arr[mid + 1])
+            r = mid;
+        else
+            l = mid + 1;
     }
 
-    printf("-1");
+    // l == r is peak
+    printf("%d", l - 1); // 0-based index
     return 0;
 }
