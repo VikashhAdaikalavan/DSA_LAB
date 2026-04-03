d958ba984b3b5d7ea16b4a0513de87219c985cc6
VikashhAdaikalavan <vikashhadaikalavan@gmail.com>
2026-04-03 14:01:42 +0530

    ...

--- Week 9/prog9.c
+++ Week 9/prog9.c
@@ -4,53 +4,185 @@
 typedef struct node
 {
     int val;
-    int colour; // 1 if for red, 0 is for black
+    int colour; // 1 = RED, 0 = BLACK
     struct node* left;
     struct node* right;
     struct node* par;
 } TreeNode;
 
-void insert(TreeNode* root, int v)
+TreeNode* createNode(int val)
 {
-    if(root->val>v)
-    {
-        if(root->left == NULL)
-        {
-            TreeNode* node = (TreeNode*)malloc(sizeof(TreeNode));
-            node->left = NULL;
-            node->right = NULL;
-            node->val = v;
-            node->colour = 1;
-            node->par = root;
-            root->left = node;
-            return ;
-        }
-        else insert(root->left,v);
-    }
+    TreeNode* node = (TreeNode*)malloc(sizeof(TreeNode));
+    node->val = val;
+    node->colour = 1; // new node is RED
+    node->left = node->right = node->par = NULL;
+    return node;
+}
+
+// LEFT ROTATE
+void lrotate(TreeNode** root, TreeNode* x)
+{
+    TreeNode* y = x->right;
+    x->right = y->left;
+
+    if(y->left != NULL)
+        y->left->par = x;
+
+    y->par = x->par;
+
+    if(x->par == NULL)
+        *root = y;
+    else if(x == x->par->left)
+        x->par->left = y;
     else
+        x->par->right = y;
+
+    y->left = x;
+    x->par = y;
+}
+
+// RIGHT ROTATE
+void rrotate(TreeNode** root, TreeNode* x)
+{
+    TreeNode* y = x->left;
+    x->left = y->right;
+
+    if(y->right != NULL)
+        y->right->par = x;
+
+    y->par = x->par;
+
+    if(x->par == NULL)
+        *root = y;
+    else if(x == x->par->left)
+        x->par->left = y;
+    else
+        x->par->right = y;
+
+    y->right = x;
+    x->par = y;
+}
+
+// FIX RED-BLACK PROPERTIES
+void fixup(TreeNode** root, TreeNode* z)
+{
+    while(z != *root && z->par->colour == 1)
     {
-        if(root->right == NULL)
+        TreeNode* parent = z->par;
+        TreeNode* grand = parent->par;
+
+        if(parent == grand->left)
         {
-            TreeNode* node = (TreeNode*)malloc(sizeof(TreeNode));
-            node->left = NULL;
-            node->right = NULL;
-            node->val = v;
-            node->colour = 1;
-            node->par = root;
-            root->right = node;
-            return ;
+            TreeNode* uncle = grand->right;
+
+            // Case 1: Uncle is RED
+            if(uncle != NULL && uncle->colour == 1)
+            {
+                parent->colour = 0;
+                uncle->colour = 0;
+                grand->colour = 1;
+                z = grand;
+            }
+            else
+            {
+                // Case 2: Left-Right
+                if(z == parent->right)
+                {
+                    z = parent;
+                    lrotate(root, z);
+                    parent = z->par;
+                }
+
+                // Case 3: Left-Left
+                parent->colour = 0;
+                grand->colour = 1;
+                rrotate(root, grand);
+                parent = z->par;
+            }
         }
-        else insert(root->right,v);
+        else
+        {
+            TreeNode* uncle = grand->left;
+
+            // Case 1: Uncle is RED
+            if(uncle != NULL && uncle->colour == 1)
+            {
+                parent->colour = 0;
+                uncle->colour = 0;
+                grand->colour = 1;
+                z = grand;
+            }
+            else
+            {
+                // Case 2: Right-Left
+                if(z == parent->left)
+                {
+                    z = parent;
+                    rrotate(root, z);
+                }
+
+                // Case 3: Right-Right
+                parent->colour = 0;
+                grand->colour = 1;
+                lrotate(root, grand);
+            }
+        }
     }
-    fixup(node);
+
+    (*root)->colour = 0; // root always BLACK
 }
 
-void rotate(TreeNode* node)
+// BST INSERT + FIXUP
+void insert(TreeNode** root, int val)
 {
-    TreeNode parent = 
+    TreeNode* z = createNode(val);
+    TreeNode* y = NULL;
+    TreeNode* x = *root;
+
+    while(x != NULL)
+    {
+        y = x;
+        if(val < x->val)
+            x = x->left;
+        else
+            x = x->right;
+    }
+
+    z->par = y;
+
+    if(y == NULL)
+        *root = z;
+    else if(val < y->val)
+        y->left = z;
+    else
+        y->right = z;
+
+    fixup(root, z);
 }
 
-void fixup(TreeNode* node)
+// MAIN
+int main()
 {
-    
+    int n, val;
+    scanf("%d", &n);
+
+    TreeNode* root = NULL;
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &val);
+        insert(&root, val);
+    }
+
+    TreeNode* temp = root;
+    int c = 0;
+    while(temp != NULL)
+    {
+        if(temp->colour == 0) c++;
+        temp = temp->left;
+    }
+
+    printf("%d \n",c);
+
+    return 0;
 }
